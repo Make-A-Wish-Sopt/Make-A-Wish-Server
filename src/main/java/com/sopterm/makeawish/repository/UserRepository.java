a6a713881fc80db779119d72e1e4f04264d608fe
yujindonut <bbb3631@naver.com>
2023-04-19 21:45:13 +0900

    [CHORE] 불필요한 의존성 제거

--- src/main/java/com/sopterm/makeawish/repository/UserRepository.java
+++ src/main/java/com/sopterm/makeawish/repository/UserRepository.java
@@ -4,9 +4,7 @@ import com.sopterm.makeawish.domain.SocialType;
 import com.sopterm.makeawish.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findBySocialTypeAndSocialId(SocialType socialType, String socialId);
