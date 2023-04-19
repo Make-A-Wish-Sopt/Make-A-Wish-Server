f11bc66207076277179e171a30f2e00f392bdee2
thguss <thgus345@naver.com>
2023-04-20 01:19:40 +0900

    [DEL] 미사용 테스트 삭제

--- src/test/java/com/sopterm/makeawish/MakeAWishApplicationTests.java
+++ src/test/java/com/sopterm/makeawish/MakeAWishApplicationTests.java
@@ -1,13 +1,7 @@
 package com.sopterm.makeawish;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MakeAWishApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
