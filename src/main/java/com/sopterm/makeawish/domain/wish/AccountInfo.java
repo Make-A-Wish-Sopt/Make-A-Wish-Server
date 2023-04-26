abdca6ba2f00f61de35bd9ff823e672b473a0755
thguss <thgus345@naver.com>
2023-04-27 03:25:35 +0900

    [ADD] 기본 생성자 추가 (에러 해결)

--- src/main/java/com/sopterm/makeawish/domain/wish/AccountInfo.java
+++ src/main/java/com/sopterm/makeawish/domain/wish/AccountInfo.java
@@ -1,10 +1,18 @@
 package com.sopterm.makeawish.domain.wish;
 
 import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
 public class AccountInfo {
 	private String name;
 	private String bank;
 	private String account;
+
+	public AccountInfo(String name, String bank, String account) {
+		this.name = name;
+		this.bank = bank;
+		this.account = account;
+	}
 }
