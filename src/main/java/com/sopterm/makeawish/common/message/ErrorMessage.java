b9328f7b999c6110788c1d1a773a492f19e727a6
thguss <thgus345@naver.com>
2023-04-27 11:34:47 +0900

    [FIX] 모호한 워딩 수정: 존재하지 않는 > 인증되지 않은

--- src/main/java/com/sopterm/makeawish/common/message/ErrorMessage.java
+++ src/main/java/com/sopterm/makeawish/common/message/ErrorMessage.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessage {
 	EXIST_MAIN_WISH("이미 진행 중인 소원 링크가 있습니다."),
-	INVALID_USER("존재하지 않는 회원입니다."),
+	INVALID_USER("인증되지 않은 회원입니다."),
 	NULL_PRINCIPAL("principal 이 null 일 수 없습니다.");
 
 	private final String message;
