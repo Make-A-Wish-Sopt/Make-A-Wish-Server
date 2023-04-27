5b5a7db4f61f016cc1f0bac20767a437a3904a71
thguss <thgus345@naver.com>
2023-04-27 11:34:32 +0900

    [FIX] 전체 생성자 롬복으로 처리

--- src/main/java/com/sopterm/makeawish/domain/wish/AccountInfo.java
+++ src/main/java/com/sopterm/makeawish/domain/wish/AccountInfo.java
@@ -1,18 +1,14 @@
 package com.sopterm.makeawish.domain.wish;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 public class AccountInfo {
 	private String name;
 	private String bank;
 	private String account;
-
-	public AccountInfo(String name, String bank, String account) {
-		this.name = name;
-		this.bank = bank;
-		this.account = account;
-	}
 }
