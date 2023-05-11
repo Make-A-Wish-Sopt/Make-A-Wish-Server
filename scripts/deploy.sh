321a356c7c269cc47f7ee47be4d2d12aedd637fe
thguss <thgus345@naver.com>
2023-05-11 15:34:24 +0900

    [DEL]  삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,12 @@ JAR_PATH=$REPOSITORY/build/libs/$APP_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z "$CURRENT_PID" ]
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 "$CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
