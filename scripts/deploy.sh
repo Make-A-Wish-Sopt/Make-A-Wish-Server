a680024491355dc5683561201772bf627f3f4192
thguss <thgus345@naver.com>
2023-05-11 15:17:27 +0900

    [FIX] 스크립트 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+REPOSITORY=/home/ubuntu/build
+# shellcheck disable=SC2164
+cd $REPOSITORY
 
-JAR_NAME=makeawish-0.0.1-SNAPSHOT.jar
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/makeawish/deploy.log/$JAR_NAME
+APP_NAME=makeawish-0.0.1-SNAPSHOT.jar
+# shellcheck disable=SC2010
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z "$CURRENT_PID" ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 "$CURRENT_PID"
+  sudo kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/makeawish/build/libs/makeawish-0.0.1-SNAPSHOT.jar
-nohup java -jar -Dspring.profiles.active=dev /home/ubuntu/makeawish/build/libs/makeawish-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포"
+nohup java -jar -DSpring.profiles.active=dev "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
