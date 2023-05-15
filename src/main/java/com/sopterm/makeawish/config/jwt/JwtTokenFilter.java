65d98bc35408f8c4e1f74a9faf7ec46e0df734d1
yangjiyoung <0112jyoung@naver.com>
2023-05-15 15:47:05 +0900

    [FIX] 토큰 검사 경로 수정

--- src/main/java/com/sopterm/makeawish/config/jwt/JwtTokenFilter.java
+++ src/main/java/com/sopterm/makeawish/config/jwt/JwtTokenFilter.java
@@ -36,7 +36,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
                 return;
             }
-            if(uri.startsWith("/api/v1/auth") || (uri.startsWith("/api/v1/cakes") && request.getMethod().equals(HttpMethod.POST)) || uri.startsWith("/v3/api-docs") ||
+            if(uri.startsWith("/api/v1/auth") || uri.equals("/api/v1/cakes") && request.getMethod().equals(HttpMethod.GET) ||(uri.startsWith("/api/v1/cakes") && request.getMethod().equals(HttpMethod.POST)) || uri.startsWith("/v3/api-docs") ||
                 uri.startsWith("/swagger-ui") || uri.startsWith("/api/v1/wishes") || uri.startsWith("/health")) {
                 filterChain.doFilter(request, response);
                 return;
