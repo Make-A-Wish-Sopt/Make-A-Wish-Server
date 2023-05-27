9679bda8f9a4daade89b06648ece5b9a8ea4c1fe
yujindonut <bbb3631@naver.com>
2023-05-28 01:19:14 +0900

    [CHORE] User ID 검색이아닌 Social Id로 수정

--- src/main/java/com/sopterm/makeawish/repository/UserRepository.java
+++ src/main/java/com/sopterm/makeawish/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package com.sopterm.makeawish.repository;
 
-import com.sopterm.makeawish.domain.user.SocialType;
 import com.sopterm.makeawish.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User findBySocialTypeAndSocialId(SocialType socialType, String socialId);
     Optional<User> findBySocialId(String socialId);
     Optional<User> findById(Long id);
 }
