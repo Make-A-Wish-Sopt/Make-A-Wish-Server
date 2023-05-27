68625a7dc31cc9b1c5792a5ced02e798247017b0
yujindonut <bbb3631@naver.com>
2023-05-27 23:54:15 +0900

    [ADD] user repository

--- src/main/java/com/sopterm/makeawish/repository/UserRepository.java
+++ src/main/java/com/sopterm/makeawish/repository/UserRepository.java
@@ -5,11 +5,14 @@ import com.sopterm.makeawish.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findBySocialTypeAndSocialId(SocialType socialType, String socialId);
     @Query("select u.id from User u")
     public Long findIdBySocialId(String socialId);
+    Optional<User> findBySocialId(String socialId);
     public boolean existsBySocialId(String socialId);
     public boolean existsByRefreshToken(String refreshToken);
 }
