4fb00ffe4cd591585fdf32cff0b643963a04fd37
yujindonut <bbb3631@naver.com>
2023-06-30 08:59:33 +0900

    [CHORE] userId 가져오는 로직 변경

--- src/main/java/com/sopterm/makeawish/controller/CakeController.java
+++ src/main/java/com/sopterm/makeawish/controller/CakeController.java
@@ -1,53 +1,47 @@
 package com.sopterm.makeawish.controller;
 
 import com.sopterm.makeawish.common.ApiResponse;
+import com.sopterm.makeawish.domain.user.InternalMemberDetails;
 import com.sopterm.makeawish.dto.present.PresentDto;
 import com.sopterm.makeawish.dto.present.PresentResponseDto;
 import com.sopterm.makeawish.service.CakeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 
-import static com.sopterm.makeawish.common.message.ErrorMessage.NULL_PRINCIPAL;
 import static com.sopterm.makeawish.common.message.SuccessMessage.SUCCESS_GET_ALL_PRESENT;
 import static com.sopterm.makeawish.common.message.SuccessMessage.SUCCESS_GET_PRESENT_MESSAGE;
-import static java.util.Objects.isNull;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/cakes")
+@SecurityRequirement(name = "Authorization")
 public class CakeController {
 
     private final CakeService cakeService;
 
     @Operation(summary = "해당 소원에 대한 케이크 결과 조회")
     @GetMapping("/{wishId}")
-    public ResponseEntity<ApiResponse> getPresents(Principal principal, @PathVariable("wishId") Long wishId) {
-        Long userId = getUserId(principal);
-        List<PresentDto> response = cakeService.getPresents(userId, wishId);
+    public ResponseEntity<ApiResponse> getPresents(@Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails,
+                                                   @PathVariable("wishId") Long wishId) {
+        List<PresentDto> response = cakeService.getPresents(memberDetails.getId(), wishId);
         return ResponseEntity.ok(ApiResponse.success(SUCCESS_GET_ALL_PRESENT.getMessage(), response));
     }
 
     @Operation(summary = "해당 소원에 대한 케이크 조회")
     @GetMapping("/{wishId}/{cakeId}")
-    public ResponseEntity<ApiResponse> getEachPresent(Principal principal, @PathVariable("wishId") Long wishId, @PathVariable("cakeId") Long cakeId) {
-        Long userId = getUserId(principal);
-        List<PresentResponseDto> response = cakeService.getEachPresent(userId, wishId, cakeId);
+    public ResponseEntity<ApiResponse> getEachPresent(@Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails,
+                                                      @PathVariable("wishId") Long wishId, @PathVariable("cakeId") Long cakeId) {
+        List<PresentResponseDto> response = cakeService.getEachPresent(memberDetails.getId(), wishId, cakeId);
         return ResponseEntity.ok(ApiResponse.success(SUCCESS_GET_PRESENT_MESSAGE.getMessage(), response));
-    }
-
-
-    private Long getUserId(Principal principal) {
-        if (isNull(principal)) {
-            throw new IllegalArgumentException(NULL_PRINCIPAL.getMessage());
-        }
-        return Long.valueOf(principal.getName());
     }
 }
--- src/main/java/com/sopterm/makeawish/controller/WishController.java
+++ src/main/java/com/sopterm/makeawish/controller/WishController.java
@@ -1,35 +1,38 @@
 package com.sopterm.makeawish.controller;
 
 import com.sopterm.makeawish.common.ApiResponse;
+import com.sopterm.makeawish.domain.user.InternalMemberDetails;
 import com.sopterm.makeawish.dto.wish.MainWishResponseDTO;
 import com.sopterm.makeawish.dto.wish.WishRequestDTO;
 import com.sopterm.makeawish.service.WishService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.security.Principal;
 
-import static com.sopterm.makeawish.common.message.ErrorMessage.NULL_PRINCIPAL;
 import static com.sopterm.makeawish.common.message.SuccessMessage.*;
-import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/wishes")
+@SecurityRequirement(name = "Authorization")
 public class WishController {
 
 	private final WishService wishService;
 
 	@Operation(summary = "소원 링크 생성")
 	@PostMapping
-	public ResponseEntity<ApiResponse> createWish(Principal principal, @RequestBody WishRequestDTO requestDTO) {
-		Long wishId = wishService.createWish(getUserId(principal), requestDTO);
+	public ResponseEntity<ApiResponse> createWish(@Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails,
+												  @RequestBody WishRequestDTO requestDTO) {
+		Long wishId = wishService.createWish(memberDetails.getId(), requestDTO);
 		return ResponseEntity
 			.created(getURI(wishId))
 			.body(ApiResponse.success(SUCCESS_CREATE_WISH.getMessage(), wishId));
@@ -37,8 +40,8 @@ public class WishController {
 
 	@Operation(summary = "메인 화면 조회")
 	@GetMapping("/main")
-	public ResponseEntity<ApiResponse> findMainWish(Principal principal) {
-		MainWishResponseDTO response = wishService.findMainWish(getUserId(principal));
+	public ResponseEntity<ApiResponse> findMainWish(@Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails) {
+		MainWishResponseDTO response = wishService.findMainWish(memberDetails.getId());
 		return nonNull(response)
 			? ResponseEntity.ok(ApiResponse.success(SUCCESS_GET_MAIN_WISH.getMessage(), response))
 			: ResponseEntity.status(NO_CONTENT).body(ApiResponse.success(NO_WISH.getMessage()));
@@ -50,13 +53,6 @@ public class WishController {
 		@RequestParam String url, @RequestParam String tag) throws Exception {
 		String response = wishService.getPresentInfo(url, tag);
 		return ResponseEntity.ok(ApiResponse.success(SUCCESS_PARSE_HTML.getMessage(), response));
-	}
-
-	private Long getUserId(Principal principal) {
-		if (isNull(principal)) {
-			throw new IllegalArgumentException(NULL_PRINCIPAL.getMessage());
-		}
-		return Long.valueOf(principal.getName());
 	}
 
 	private URI getURI(Long id) {
