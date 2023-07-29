f11fe0b96f750e6127c2982cc8d8fc13b68cf471
thguss <thgus345@naver.com>
2023-07-30 00:55:06 +0900

    [FIX] cake id 필드명 변경

--- src/main/java/com/sopterm/makeawish/dto/cake/CakeApproveRequestDTO.java
+++ src/main/java/com/sopterm/makeawish/dto/cake/CakeApproveRequestDTO.java
@@ -3,13 +3,13 @@ package com.sopterm.makeawish.dto.cake;
 import lombok.Builder;
 
 @Builder
-public record CakeApproveRequestDto(
+public record CakeApproveRequestDTO(
         String pgToken,
         String tid,
         String partnerOrderId,
         String partnerUserId,
         String name,
-        Long cake,
+        Long cakeId,
         String message,
         Long wishId
 ) {
