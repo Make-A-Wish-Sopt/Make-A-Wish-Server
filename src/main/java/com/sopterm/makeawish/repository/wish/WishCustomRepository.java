6334c2c7feadf91a3dda586fbc75695f5d3051e0
thguss <thgus345@naver.com>
2023-07-30 22:20:37 +0900

    [ADD] WishRepository 커스텀 추가

---
+++ src/main/java/com/sopterm/makeawish/repository/wish/WishCustomRepository.java
@@ -0,0 +1,13 @@
+package com.sopterm.makeawish.repository.wish;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import com.sopterm.makeawish.domain.user.User;
+import com.sopterm.makeawish.domain.wish.Wish;
+
+public interface WishCustomRepository {
+
+	Optional<Wish> findMainWish(User wisher, int expiryDay);
+	boolean existsConflictWish(User wisher, LocalDateTime startAt, LocalDateTime endAt, int expiryDay);
+}
---
+++ src/main/java/com/sopterm/makeawish/repository/wish/WishRepositoryImpl.java
@@ -0,0 +1,58 @@
+package com.sopterm.makeawish.repository.wish;
+
+import static com.sopterm.makeawish.domain.wish.QWish.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sopterm.makeawish.domain.user.User;
+import com.sopterm.makeawish.domain.wish.Wish;
+
+import lombok.*;
+
+@Repository
+@RequiredArgsConstructor
+public class WishRepositoryImpl implements WishCustomRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public Optional<Wish> findMainWish(User wisher, int expiryDay) {
+		val now = LocalDateTime.now();
+		return Optional.ofNullable(queryFactory
+			.select(wish)
+			.from(wish)
+			.where(
+				wish.wisher.eq(wisher),
+				wish.startAt.before(now),
+				wish.endAt.after(now.minusDays(expiryDay))
+			)
+			.fetchFirst()
+		);
+	}
+
+	@Override
+	public boolean existsConflictWish(User wisher, LocalDateTime startAt, LocalDateTime endAt, int expiryDay) {
+		return queryFactory
+			.select(wish)
+			.from(wish)
+			.where(
+				wish.wisher.eq(wisher),
+				conflictTerm(startAt, endAt, expiryDay)
+			)
+			.fetchFirst() != null;
+	}
+
+	private BooleanBuilder conflictTerm(LocalDateTime from, LocalDateTime to, int expiryDay) {
+		val booleanBuilder = new BooleanBuilder();
+		booleanBuilder.or(wish.startAt.between(from, to));
+		booleanBuilder.or(wish.endAt.between(from, to));
+		booleanBuilder.or(wish.endAt.after(from.minusDays(expiryDay)));
+		booleanBuilder.or(wish.endAt.eq(from.minusDays(expiryDay)));
+		return booleanBuilder;
+	}
+}
