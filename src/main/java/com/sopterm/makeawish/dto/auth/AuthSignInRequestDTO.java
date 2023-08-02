d87ebdbf78c6152fd4bb99c9cbe31d5cb34321b8
thguss <thgus345@naver.com>
2023-08-02 21:13:47 +0900

    [REFACT] 통일화 리팩토링

--- src/main/java/com/sopterm/makeawish/dto/auth/AuthSignInRequestDTO.java
+++ src/main/java/com/sopterm/makeawish/dto/auth/AuthSignInRequestDTO.java
@@ -1,6 +1,7 @@
 package com.sopterm.makeawish.dto.auth;
 
 import com.sopterm.makeawish.domain.user.SocialType;
+
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.NonNull;
@@ -9,24 +10,30 @@ import java.time.LocalDateTime;
 
 @Builder
 public record AuthSignInRequestDTO(
-        @Email
-        String email,
-        @NonNull
-        SocialType socialType,
-        @NonNull
-        String socialId,
-        @NonNull
-        String nickname,
-        @NonNull
-        LocalDateTime createdAt
-){
-        public static AuthSignInRequestDTO to(String email, SocialType socialType, String socialId, String nickname, LocalDateTime createdAt) {
-                return AuthSignInRequestDTO.builder()
-                        .email(email)
-                        .socialType(socialType)
-                        .nickname(nickname)
-                        .socialId(socialId)
-                        .createdAt(createdAt)
-                        .build();
-        }
+	@Email
+	String email,
+	@NonNull
+	SocialType socialType,
+	@NonNull
+	String socialId,
+	@NonNull
+	String nickname,
+	@NonNull
+	LocalDateTime createdAt
+) {
+	public static AuthSignInRequestDTO to(
+		String email,
+		SocialType socialType,
+		String socialId,
+		String nickname,
+		LocalDateTime createdAt
+	) {
+		return AuthSignInRequestDTO.builder()
+			.email(email)
+			.socialType(socialType)
+			.nickname(nickname)
+			.socialId(socialId)
+			.createdAt(createdAt)
+			.build();
+	}
 }
