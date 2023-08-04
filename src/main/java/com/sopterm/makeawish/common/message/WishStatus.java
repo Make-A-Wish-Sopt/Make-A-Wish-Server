37d9be545c30fdd79e89601bd1bab3847e2cf4e8
yujindonut <bbb3631@naver.com>
2023-08-04 16:14:49 +0900

    [CHORE] Enum값 값 정보 수정

--- src/main/java/com/sopterm/makeawish/common/message/WishStatus.java
+++ src/main/java/com/sopterm/makeawish/common/message/WishStatus.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum WishStatus {
-    BEFORE(1),
-    WHILE(2),
-    END(3);
-
-    private final int status;
+    BEFORE,
+    WHILE,
+    END;
 }
