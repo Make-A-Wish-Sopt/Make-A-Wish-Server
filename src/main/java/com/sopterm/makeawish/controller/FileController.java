f5a55a3f79b64cde166762f8ebd105924ebcf082
yujindonut <bbb3631@naver.com>
2023-08-10 15:52:42 +0900

    [CHORE] API response 수정

--- src/main/java/com/sopterm/makeawish/controller/FileController.java
+++ src/main/java/com/sopterm/makeawish/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.sopterm.makeawish.controller;
 
+import com.sopterm.makeawish.common.ApiResponse;
 import com.sopterm.makeawish.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -7,14 +8,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import static com.sopterm.makeawish.common.message.SuccessMessage.SUCCESS_GET_S3_URL;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,10 +34,10 @@ public class FileController {
 
     @Operation(summary = "이미지 업로드를 위한 url 관련 API")
     @GetMapping("")
-    public ResponseEntity<Map<String, String>> getPresignedUrl(
+    public ResponseEntity<ApiResponse> getPresignedUrl(
             @RequestParam String fileName
     ) {
-        val response = fileService.getSignedUrl(activeProfile, imageBucketName, fileName);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        val response = fileService.getSignedUrl(activeProfile, imageBucketName, imageBucketPath, fileName);
+        return ResponseEntity.ok(ApiResponse.success(SUCCESS_GET_S3_URL.getMessage(), response));
     }
 }
