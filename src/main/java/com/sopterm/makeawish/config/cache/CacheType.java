7f8982a1b973980ac217c27f1af45ee38680449b
yang Jiyoung <0112jyoung@naver.com>
2023-09-20 22:53:09 +0900

    [FEAT] 캐시 매니저 생성

---
+++ src/main/java/com/sopterm/makeawish/config/cache/CacheType.java
@@ -0,0 +1,15 @@
+package com.sopterm.makeawish.config.cache;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CacheType {
+
+    PRESENT_COUNT("presents", 3000, 30);
+
+    private final String cacheName;
+    private final int maxSize;
+    private final int timeToLive;
+}
