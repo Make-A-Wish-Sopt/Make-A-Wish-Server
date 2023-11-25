f37f6b28ed225487b619948bff28616dea0d158b
yang Jiyoung <0112jyoung@naver.com>
2023-11-25 20:47:48 +0900

    [FEAT] 암호화 iv값 추가

--- src/main/java/com/sopterm/makeawish/config/crypto/KeyEncrypt.java
+++ src/main/java/com/sopterm/makeawish/config/crypto/KeyEncrypt.java
@@ -1,15 +1,18 @@
 package com.sopterm.makeawish.config.crypto;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -20,23 +23,31 @@ import java.util.Base64;
 public class KeyEncrypt {
     private static final String ALGORITHM = "AES";
     private static final Charset ENCODING_TYPE = StandardCharsets.UTF_8;
-    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
-    private final String secretKey = "tjqjsecretkey0306!";
+    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
 
+    @Value("${crypto.secret-key}")
+    private String secretKey;
 
-    public String encrpyt(String plainText) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    @Value("${crypto.iv}")
+    private String iv;
+
+    public String encrypt(String plainText) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(ENCODING_TYPE), ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
         byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(ENCODING_TYPE));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    public String decrypt(String encryptedText) throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+    public String decrypt(String encryptedText) throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), ALGORITHM);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-        return new String(decryptedBytes, StandardCharsets.UTF_8);
+        return new String(decryptedBytes, ENCODING_TYPE);
     }
 }
