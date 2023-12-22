0666802790e36513e3db49677463f777dca16e38
yang Jiyoung <0112jyoung@naver.com>
2023-12-22 18:24:15 +0900

    [FIX] 계좌 정보 추가 전달

--- src/main/java/com/sopterm/makeawish/dto/wish/WishResponseDTO.java
+++ src/main/java/com/sopterm/makeawish/dto/wish/WishResponseDTO.java
@@ -9,17 +9,27 @@ import java.time.temporal.ChronoUnit;
 import com.sopterm.makeawish.domain.wish.Wish;
 
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 
 @Builder
-public record WishResponseDTO(String name, long dayCount, String title, String hint) {
+public record WishResponseDTO(String name, long dayCount, String title, String hint, String bank, String accountNumber) {
 
 	public static WishResponseDTO from(Wish wish) {
 		val name = nonNull(wish.getWisher().getAccount())
 			? wish.getWisher().getAccount().getName()
 			: wish.getWisher().getNickname();
 
+		val account = nonNull(wish.getWisher().getAccount())
+				? wish.getWisher().getAccount().getAccount()
+				: StringUtils.EMPTY;
+		val bank = nonNull(wish.getWisher().getAccount())
+				? wish.getWisher().getAccount().getBank()
+				: StringUtils.EMPTY;
+
 		return WishResponseDTO.builder()
 			.name(name)
+			.accountNumber(account)
+			.bank(bank)
 			.dayCount(getRemainDayCount(wish.getEndAt()))
 			.title(wish.getTitle())
 			.hint(wish.getHint())
