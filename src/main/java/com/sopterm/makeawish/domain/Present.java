ebed03826cf4a2097be0b3dffc992c074939123b
yang Jiyoung <0112jyoung@naver.com>
2024-02-06 18:35:16 +0900

    [FIX] 줄 정렬

--- src/main/java/com/sopterm/makeawish/domain/Present.java
+++ src/main/java/com/sopterm/makeawish/domain/Present.java
@@ -1,19 +1,17 @@
 package com.sopterm.makeawish.domain;
 
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.*;
-import static java.util.Objects.*;
-
 import com.sopterm.makeawish.domain.wish.Wish;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.util.Objects.nonNull;
 
 @Getter
 @Entity
@@ -21,79 +19,81 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class Present {
 
-	@Id @GeneratedValue(strategy = IDENTITY)
-	@Column(name = "present_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "present_id")
+    private Long id;
 
-	private String name;
+    private String name;
 
-	@Column(columnDefinition = "TEXT")
-	private String message;
+    @Column(columnDefinition = "TEXT")
+    private String message;
 
-	@ManyToOne(fetch = LAZY)
-	@JoinColumn(name = "wish_id")
-	private Wish wish;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "wish_id")
+    private Wish wish;
 
-	@ManyToOne(fetch = LAZY)
-	@JoinColumn(name = "cake_id")
-	private Cake cake;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "cake_id")
+    private Cake cake;
 
-	@CreatedDate
-	protected LocalDateTime createdAt;
+    @CreatedDate
+    protected LocalDateTime createdAt;
 
-	public static class PresentBuilder {
-		private String name;
-		private String message;
-		private Wish wish;
-		private Cake cake;
+    public static class PresentBuilder {
+        private String name;
+        private String message;
+        private Wish wish;
+        private Cake cake;
 
-		private PresentBuilder() {}
+        private PresentBuilder() {
+        }
 
-		public static PresentBuilder builder() {
-			return new PresentBuilder();
-		}
+        public static PresentBuilder builder() {
+            return new PresentBuilder();
+        }
 
-		public PresentBuilder name(String name) {
-			this.name = name;
-			return this;
-		}
+        public PresentBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
 
-		public PresentBuilder message(String message) {
-			this.message = message;
-			return this;
-		}
+        public PresentBuilder message(String message) {
+            this.message = message;
+            return this;
+        }
 
-		public PresentBuilder wish(Wish wish) {
-			this.wish = wish;
-			return this;
-		}
+        public PresentBuilder wish(Wish wish) {
+            this.wish = wish;
+            return this;
+        }
 
-		public PresentBuilder cake(Cake cake) {
-			this.cake = cake;
-			return this;
-		}
+        public PresentBuilder cake(Cake cake) {
+            this.cake = cake;
+            return this;
+        }
 
-		private void setWish(Present present, Wish wish) {
-			if (nonNull(present.wish)) {
-				present.wish.getPresents().remove(present);
-			}
-			present.wish = wish;
-			if (nonNull(wish)) {
-				wish.getPresents().add(present);
-			}
-		}
+        private void setWish(Present present, Wish wish) {
+            if (nonNull(present.wish)) {
+                present.wish.getPresents().remove(present);
+            }
+            present.wish = wish;
+            if (nonNull(wish)) {
+                wish.getPresents().add(present);
+            }
+        }
 
-		public Present build() {
-			Present present = new Present();
-			present.name = this.name;
-			present.message = this.message;
-			setWish(present, this.wish);
-			present.cake = this.cake;
-			return present;
-		}
-	}
+        public Present build() {
+            Present present = new Present();
+            present.name = this.name;
+            present.message = this.message;
+            setWish(present, this.wish);
+            present.cake = this.cake;
+            return present;
+        }
+    }
 
-	public static PresentBuilder builder() {
-		return new PresentBuilder();
-	}
+    public static PresentBuilder builder() {
+        return new PresentBuilder();
+    }
 }
