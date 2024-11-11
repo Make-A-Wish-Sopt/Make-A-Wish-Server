0abf1700b079cdb6515a6988a93482fc5b1ede0a
yang Jiyoung <0112jyoung@naver.com>
2024-11-11 10:39:31 +0900

    [FIX] 소원 조회 시 힌트

--- src/main/java/com/sopterm/makeawish/dto/wish/WishResponseDTO.java
+++ src/main/java/com/sopterm/makeawish/dto/wish/WishResponseDTO.java
@@ -32,6 +32,7 @@ public record WishResponseDTO(String name, long dayCount, String title, String h
 			.bank(bank)
 			.dayCount(getRemainDayCount(wish.getEndAt()))
 			.title(wish.getTitle())
+			.hint(wish.getHint())
 			.wantsGift(wish.isWantsGift())
 			.presentImageUrl(wish.getPresentImageUrl())
 			.build();
