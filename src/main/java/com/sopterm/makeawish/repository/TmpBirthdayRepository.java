85e3312b35e99efce89e4aef4733e2c4cb5d9a2b
yang Jiyoung <0112jyoung@naver.com>
2025-04-09 18:56:23 +0900

    [FIX] 생일 - 전화번호 중복 입력 시 무시하도록 수정

--- src/main/java/com/sopterm/makeawish/repository/TmpBirthdayRepository.java
+++ src/main/java/com/sopterm/makeawish/repository/TmpBirthdayRepository.java
@@ -2,6 +2,13 @@ package com.sopterm.makeawish.repository;
 
 import com.sopterm.makeawish.domain.TmpBirthday;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TmpBirthdayRepository extends JpaRepository<TmpBirthday, Long> {
+    @Modifying
+    @Query(value = "INSERT INTO tmp_birthday (birth_date, phone_number) VALUES (:birthDate, :phoneNumber) " +
+            "ON CONFLICT (birth_date, phone_number) DO NOTHING", nativeQuery = true)
+    void saveOrUpdateBirthday(@Param("birthDate") String birthDate, @Param("phoneNumber") String phoneNumber);
 }
